d36d177b3bf000fa12ec3cf6b3207ce1011a1e15
David Toro Alzate <dtoro@cognox.com>
2016-11-04 12:01:25 -0500

    para la casa

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ var index = new Array();
 $.each($(json)[0], function (key) {
     index.push(key);
 });
+index.pop();
 
 var app = angular.module('UazonApp', []);
 
@@ -13,3 +14,18 @@ function mcontrolador($scope){
     $scope.index = index;
 }
 app.controller('mcontroladorH', mcontrolador);
+
+var app2 = angular.module('UazonApp2',["ngRoute"]);
+
+app2.config(function($routeProvider){
+    $routeProvider
+        .when("/listadoIndex",{
+            templateUrl: "listadoIndex.html"
+        })
+        .when("/listado",{
+        templateUrl: "listado.html"
+    })
+        .when("/fichaHTML", {
+            templateUrl: "ficha.html"
+        });
+});
